751ac65daa28371fe767ab0f5fa11f4cf957d621
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-12 09:35:23 -0700

    Fix <c-t> open in tab for Telescope

--- confs/neovim/.config/nvim/lua/plugins/telescope.lua
+++ confs/neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,6 @@
+local actions = require("telescope.actions")
+local Util = require("lazyvim.util")
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -27,16 +30,41 @@ return {
         desc = "Find in files",
       },
     },
-    opts = {
-      defaults = {
-        prompt_prefix = " ",
-        selection_caret = " ",
-        mappings = {
-          i = {
-            ["<C-q>"] = require("telescope.actions").smart_send_to_qflist + require("telescope.actions").open_qflist,
-          },
+    opts = function(_, opts)
+      opts.defaults.mappings = {
+        i = {
+          ["<c-h>"] = "which_key",
+          ["<c-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+          ["<c-t"] = actions.select_tab,
+          ["<a-i>"] = function()
+            local action_state = require("telescope.actions.state")
+            local line = action_state.get_current_line()
+            Util.telescope("find_files", { no_ignore = true, default_text = line })()
+          end,
+          ["<a-h>"] = function()
+            local action_state = require("telescope.actions.state")
+            local line = action_state.get_current_line()
+            Util.telescope("find_files", { hidden = true, default_text = line })()
+          end,
+          ["<C-Down>"] = function(...)
+            return require("telescope.actions").cycle_history_next(...)
+          end,
+          ["<C-Up>"] = function(...)
+            return require("telescope.actions").cycle_history_prev(...)
+          end,
+          ["<C-f>"] = function(...)
+            return require("telescope.actions").preview_scrolling_down(...)
+          end,
+          ["<C-b>"] = function(...)
+            return require("telescope.actions").preview_scrolling_up(...)
+          end,
         },
-      },
-    },
+        n = {
+          ["q"] = function(...)
+            return require("telescope.actions").close(...)
+          end,
+        },
+      }
+    end,
   },
 }
