1a403c6b0d2a383b9d5b1d43081a1b946ebec082
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-12 18:45:14 -0700

    Enable LSP for Node/Typescript projects

--- confs/neovim/.config/nvim/lua/config/lazy.lua
+++ confs/neovim/.config/nvim/lua/config/lazy.lua
@@ -11,10 +11,11 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- TODO: import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    -- enable copilot
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
