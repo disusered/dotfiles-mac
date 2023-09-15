47695ac4456a1acf4c57f5c4b5b537eb5b1b4eb7
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-15 11:12:16 -0700

    Add more Treesitter syntax

---
+++ confs/neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "cpp",
+        "cmake",
+        "css",
+        "graphql",
+        "latex",
+        "make",
+        "scss",
+        "toml",
+        "vue",
+        "yaml",
+      })
+    end,
+  },
+}
