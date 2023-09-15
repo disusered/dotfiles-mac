4b63d6ec707b71f4628e739a43fe5b51f976fad1
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-15 10:40:20 -0700

    Add more Mason plugins

---
+++ confs/neovim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,14 @@
+return {
+  {
+
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "bash-language-server",
+        "css-lsp",
+        "html-lsp",
+        "vue-language-server",
+      },
+    },
+  },
+}
