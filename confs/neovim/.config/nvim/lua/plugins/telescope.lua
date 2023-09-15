c0e194fd045b37c9ddedf4064c91f645dacf219e
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-15 07:28:21 -0700

    Fix grep options to find hidden files

--- confs/neovim/.config/nvim/lua/plugins/telescope.lua
+++ confs/neovim/.config/nvim/lua/plugins/telescope.lua
@@ -36,6 +36,21 @@ return {
       },
     },
     opts = function(_, opts)
+      opts.defaults.vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",
+        "--glob",
+        "!.git",
+        "--glob",
+        "!node_modules",
+      }
+
       opts.defaults.mappings = {
         i = {
           ["<c-h>"] = "which_key",
