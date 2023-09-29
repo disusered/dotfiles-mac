bab32e344b2af64b96553f486917bdd01ae76dd8
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-29 08:15:18 -0700

    Enable prettier and disable DAP

--- confs/neovim/.config/nvim/lua/config/lazy.lua
+++ confs/neovim/.config/nvim/lua/config/lazy.lua
@@ -19,10 +19,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.dap.core" },
     -- doesn't work with eslint-plugin-prettier
-    -- { import = "lazyvim.plugins.extras.linting.eslint" },
-    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
     { import = "plugins" },
   },
