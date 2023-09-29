062503fe2d682563cd49990611acd88a6e0425c0
Carlos Rosquillas <crosquillas@gmail.com>
2023-09-29 08:15:45 -0700

    Configure persistent undos

--- confs/neovim/.config/nvim/lua/config/options.lua
+++ confs/neovim/.config/nvim/lua/config/options.lua
@@ -57,3 +57,6 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
+
+-- Set undofile for persistent undo
+vim.o.undofile = "~/.config/nvim/undos/"
