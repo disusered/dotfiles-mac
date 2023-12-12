c43c82014975e8e635516a7aa8e8438de22a1659
Carlos Rosquillas <crosquillas@gmail.com>
2023-12-12 15:37:22 -0800

    Add Neotest for Elixir

--- confs/neovim/.config/nvim/lua/plugins/neotest.lua
+++ confs/neovim/.config/nvim/lua/plugins/neotest.lua
@@ -1,10 +1,12 @@
 return {
   { "nvim-neotest/neotest-jest" },
+  { "jfpedroza/neotest-elixir" },
   {
     "nvim-neotest/neotest",
     config = function(_, opts)
       vim.list_extend(opts.adapters, {
         require("neotest-jest"),
+        require("neotest-elixir"),
       })
       require("neotest").setup(opts)
     end,
