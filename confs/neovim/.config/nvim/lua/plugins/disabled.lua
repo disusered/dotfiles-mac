86ac0f4380c7eb7705130fb964dab46d7e4dbbab
Carlos Rosquillas <crosquillas@gmail.com>
2023-12-11 18:35:54 -0800

    Enable more UI nice-to-haves

--- confs/neovim/.config/nvim/lua/plugins/disabled.lua
+++ confs/neovim/.config/nvim/lua/plugins/disabled.lua
@@ -1,32 +1,34 @@
 return {
   -- disable bufferline
-  { "akinsho/bufferline.nvim", enabled = false },
+  { "akinsho/bufferline.nvim",             enabled = false },
   -- disable fancy start screen
-  { "nvimdev/dashboard-nvim", enabled = false },
+  { "nvimdev/dashboard-nvim",              enabled = false },
   -- disable visual aids on fFtT and /
-  { "folke/flash.nvim", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
+  { "folke/flash.nvim",                    enabled = false },
+  { "ggandor/flit.nvim",                   enabled = false },
   -- remove excessive indent cues
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
   -- no automatic pairs
-  { "mini.pairs", enabled = false },
+  { "mini.pairs",                          enabled = false },
   -- no session management
-  { "folke/persistence.nvim", enabled = false },
+  { "folke/persistence.nvim",              enabled = false },
   -- no project management
-  { "ahmedkhalf/project.nvim", enabled = false },
+  { "ahmedkhalf/project.nvim",             enabled = false },
   -- don't use search/replace plugin
-  { "nvim-pack/nvim-spectre", enabled = false },
+  { "nvim-pack/nvim-spectre",              enabled = false },
   -- don't use mini.surround
-  { "echasnovski/mini.surround", enabled = false },
+  { "echasnovski/mini.surround",           enabled = false },
   -- disable tokyonight
-  { "folke/tokyonight.nvim", enabled = false },
+  { "folke/tokyonight.nvim",               enabled = false },
   -- disable UI extras
-  { "stevearc/dressing.nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  { "folke/edgy.nvim", enabled = false },
+  { "stevearc/dressing.nvim",              enabled = true },
+  { "rcarriga/nvim-notify",                enabled = true },
+  { "folke/noice.nvim",                    enabled = false },
+  { "folke/edgy.nvim",                     enabled = false },
   -- disable redundant highlighting
-  { "RRethy/vim-illuminate", enabled = false },
+  { "RRethy/vim-illuminate",               enabled = false },
   -- no file explorer
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim",         enabled = false },
+  -- fuck conform.nvim
+  { "stevearc/conform.nvim",               enabled = false },
 }
