e53da29169acd6e88d8825149eb4cc2e13c6bfad
Carlos Rosquillas <crosquillas@gmail.com>
2024-03-26 11:56:41 -0700

    Allow unit testing python files

--- confs/neovim/.config/nvim/lua/plugins/neotest.lua
+++ confs/neovim/.config/nvim/lua/plugins/neotest.lua
@@ -1,12 +1,14 @@
 return {
   { "nvim-neotest/neotest-jest" },
   { "jfpedroza/neotest-elixir" },
+  { "nvim-neotest/neotest-python" },
   {
     "nvim-neotest/neotest",
     config = function(_, opts)
       vim.list_extend(opts.adapters, {
         require("neotest-jest"),
         require("neotest-elixir"),
+        require("neotest-python"),
       })
       require("neotest").setup(opts)
     end,
