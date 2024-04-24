556f5f2b4405699fae5f9d956bd2b900d3285714
Carlos Rosquillas <crosquillas@gmail.com>
2024-04-24 13:27:32 -0700

    Update Markdown Preview with keymap

--- confs/neovim/.config/nvim/lua/plugins/markdown.lua
+++ confs/neovim/.config/nvim/lua/plugins/markdown.lua
@@ -1,21 +1,43 @@
 return {
   {
-    -- TODO: Use toppair/peek.nvim when this is merged
-    "Saimo/peek.nvim",
+    "toppair/peek.nvim",
     event = { "VeryLazy" },
     build = "deno task --quiet build:fast",
+
     config = function()
       require("peek").setup({
-        auto_load = false, -- whether to automatically load preview when entering another markdown buffer
-        close_on_bdelete = true, -- close preview window on buffer delete
-        syntax = true, -- enable syntax highlighting, affects performance
-        theme = "light", -- 'dark' or 'light'
+        auto_load = false,         -- whether to automatically load preview when entering another markdown buffer
+        close_on_bdelete = true,   -- close preview window on buffer delete
+        syntax = true,             -- enable syntax highlighting, affects performance
+        theme = "light",           -- 'dark' or 'light'
         update_on_change = true,
-        app = "webview", -- 'webview', 'browser', string or a table of strings
+        app = "webview",           -- 'webview', 'browser', string or a table of strings
         filetype = { "markdown" }, -- list of filetypes to recognize as markdown
       })
       vim.api.nvim_create_user_command("MarkdownPreviewOpen", require("peek").open, {})
       vim.api.nvim_create_user_command("MarkdownPreviewClose", require("peek").close, {})
     end,
+
+    ft = { "markdown" },
+
+    keys = function()
+      local peek = require("peek")
+      local peek_toggle = function()
+        if peek.is_open() then
+          peek.close()
+        else
+          peek.open()
+        end
+      end
+
+      return {
+        {
+          "<leader>cp",
+          function() peek_toggle() end,
+          desc = "Toggle markdown preview",
+        },
+      }
+    end
+
   },
 }
