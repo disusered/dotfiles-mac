7884c13c9c4ca4f331140b9b66615fd82414c4e8
Carlos Rosquillas <crosquillas@gmail.com>
2024-07-09 17:48:38 -0700

    Update LazyVim configs

--- confs/neovim/.config/nvim/lua/config/lazy.lua
+++ confs/neovim/.config/nvim/lua/config/lazy.lua
@@ -10,7 +10,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim",                                    import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- add coding/ide features
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
